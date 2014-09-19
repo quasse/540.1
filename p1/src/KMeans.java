49c74c2c5d60e7410cf81a6acb4f2afffd726d96
quasse <quasse@gmail.com>
2014-09-19 11:37:04 -0500

    Worked on the assignCentroids method. Got it pretty much complete except for need to finish the findOrphans method.

--- p1/src/KMeans.java
+++ p1/src/KMeans.java
@@ -36,17 +36,39 @@ public class KMeans {
 		 //Array for holding the difference between a centroid and an instance.
 		 double[][]differences = new double[centroids.length][instances[0].length];
 		 
+		 /*
+		  * Iterates through each instance, storing the difference between an 
+		  * instance's feature location and the centroid's feature location
+		  * in an array, differences[][]. Then, the method getClosest() returns
+		  * whichever centroid is closest, and that centroid is assigned the 
+		  * instance.
+		  */
 		 for (int i = 0; i < instances.length; i++){
 			 for(int j = 0; j < centroids.length; j++){
 				 for (int k = 0; k < centroids[j].length; k++){
+					 /*
+					  * Because it is a given that each instances[][] is as long
+					  * as centroids[][], we can use the same variable to
+					  * maneuver "horizontally" along each array.
+					  */
 					 differences[j][k] = centroids[j][k] - instances[i][k];
+				 }//end for
+			 }//end for
+			 
+			 //Iterates through the assignments array, placing the assigned
+			 //instance in the first empty space of whatever row the centroid 
+			 //corresponds to.
+			 int centroidAssignment = getClosest(differences);
+			 for (int l = 0; l < assignments[centroidAssignment].length; l++){
+				 if (assignments[centroidAssignment][l] == 0){
+					 assignments[centroidAssignment][l] = i;
 				 }
-				getClosest(differences);
 			 }
-		 }
+		 }//end for
+		 findOrphans(assignments, centroids, instances);
 		 moveCentroids(centroids, assignments);
 		 return centroids;
-	 }
+	 }//end method
 
 	 /*
 	  * Moves all the centroids according to their new assignments and finds the 
@@ -58,7 +80,49 @@ public class KMeans {
 		 return centroids;
 	 }
 	 
-	 private double getClosest(double[][] differences){
+	 /*
+	  * Returns whatever centroid is closest to the instance.
+	  * The centroid is indicated by its row.
+	  */
+	 private int getClosest(double[][] differences){
+		 
+		 //Array to store the distances between each centroid and instance
+		 double[] totalDistance = new double[differences.length];
+		 
+		 //Will indicate which element of totalDistance is the smallest
+		 int indicator = 0;
+		 
+		 //Iterates through differences[][], adding each difference to the next
+		 for (int i = 0; i < differences.length; i++){
+			 for (int j = 0; j < differences[i].length; j++){
+				 totalDistance[i] = totalDistance[i] + differences[i][j];
+			 }
+		 }
+		 
+		 
+		 double min = totalDistance[0];
+		 
+		 //Sees which element in totalDistance is the smallest
+		 for (int i = 1; i < totalDistance.length; i++){
+			 if (totalDistance[i] < min ){
+				 indicator = i;
+			 }//end if
+		 }//end for
+		 return indicator;
+	 }//end method
+	 
+	 /*
+	  * Searhces through the assignments array and looks to see if any of the 
+	  * rows are empty.
+	  */
+	 private void findOrphans(double assignments[][], double centroids[][],
+			 double instances[][]){
+		 
+		 for (int i = 0; i < assignments.length; i ++){
+			 if (assignments[i][0] == 0){
+				 //to do
+			 }
+		 }
 		 
 	 }
 }
